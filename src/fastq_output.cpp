c599861eaf443c4548ba0c0b4ffcc92010da419d
Finlay Maguire <finlaymaguire@gmail.com>
2015-02-13 13:47:18 +0000

    Add cluster size output

--- src/fastq_output.cpp
+++ src/fastq_output.cpp
@@ -25,6 +25,9 @@ void split_fastq(arma::Col<size_t> assignments, char* r1_fn, char* r2_fn) {
     unsigned int line_number = 0;
     unsigned int read_number = 0;
 
+    unsigned int cluster0_tally = 0;
+    unsigned int cluster1_tally = 0;
+
     if(r1_fp.is_open() && r2_fp.is_open()){
         while(std::getline(r1_fp, r1_line), std::getline(r2_fp, r2_line)){
  
@@ -35,11 +38,15 @@ void split_fastq(arma::Col<size_t> assignments, char* r1_fn, char* r2_fn) {
             if(assignments[read_number] == 0){
                 cluster0_r1_fp << r1_line << '\n';
                 cluster0_r2_fp << r2_line << '\n';
+
+                cluster0_tally++;
             }
 
             if(assignments[read_number] == 1){
                 cluster1_r1_fp << r1_line << '\n';
                 cluster1_r2_fp << r2_line << '\n';
+
+                cluster1_tally++;
             }
             
             line_number++;
@@ -53,4 +60,8 @@ void split_fastq(arma::Col<size_t> assignments, char* r1_fn, char* r2_fn) {
     cluster0_r2_fp.close();
     cluster1_r1_fp.close();    
     cluster1_r2_fp.close();
+    
+    std::cout << "Reads assigned to cluster 0: " << cluster0_tally/4 << '\n';
+    std::cout << "Reads assigned to cluster 1: " << cluster1_tally/4 << '\n';
+
 }
